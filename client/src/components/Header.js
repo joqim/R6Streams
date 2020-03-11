ab9b9d180ab8be2329e71717c01baddfdb818e73
Dhipauk27 <dhipauk27@gmail.com>
2020-03-11 14:33:33 +0530

    trying to push

---
+++ client/src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link }  from 'react-router-dom';
+import GoogleAuth from './GoogleAuth';
+
+const Header = () => {
+  return (
+    <div>
+      <div className="ui secondary pointing menu">
+        <Link to="/" className="item">
+          R6BET
+        </Link>
+        <div className="right menu">
+          <Link to="/" className="item">
+            View Streams
+          </Link>
+          <GoogleAuth />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
